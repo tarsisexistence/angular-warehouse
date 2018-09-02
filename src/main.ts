7dc68efa21373d240b947fdb80c1ac119c430953
maktarsis <tarsis.maksym@gmail.com>
2018-09-02 15:09:34 +0300

    feat: implement hot module reloading

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import 'hammerjs';
+import {
+  ApplicationRef,
+  enableProdMode
+} from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableDebugTools } from '@angular/platform-browser';
 
+import { hmrBootstrap } from './main.hmr';
 import { AppModule } from '@app/app.module';
 import { environment } from '@env/environment';
 
@@ -12,7 +17,32 @@ if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (!environment.hmr) {
+  bootstrap()
+      .then(ref => {
+        if (environment.production) {
+          return;
+        }
+
+        const applicationRef = ref.injector.get(ApplicationRef);
+        const appComponent = applicationRef.components[0];
+        enableDebugTools(appComponent);
+      })
       .catch(err => console.log(err));
-});
+} else {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+    console.log('hmr is on');
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+}
+
+// TODO: bootstrap on the server
+// document.addEventListener('DOMContentLoaded', () => {
+//   platformBrowserDynamic().bootstrapModule(AppModule)
+//       .catch(err => console.log(err));
+// });

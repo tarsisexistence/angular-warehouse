21c20de30f04a3c2c3a1f57444012b29229182bd
maktarsis <tarsis.maksym@gmail.com>
2018-09-03 18:34:21 +0300

    refactor: hmr

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -8,22 +8,21 @@ import { setupNgProfiler } from './profiler';
 export const hmrBootstrap = (
     module: any,
     bootstrap: () => Promise<NgModuleRef<any>>
-) => {
+): void => {
   let ngModule: NgModuleRef<any>;
 
   module.hot.accept();
+  module.hot.dispose(() => {
+    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+    const elements = appRef.components.map((c: any) => c.location.nativeElement);
+    const makeVisible = createNewHosts(elements);
+
+    ngModule.destroy();
+    makeVisible();
+  });
 
   bootstrap().then((reference: any) => {
     ngModule = reference;
-
     setupNgProfiler(reference);
-  });
-
-  module.hot.dispose(() => {
-    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
-    const elements = appRef.components.map(c => c.location.nativeElement);
-    const makeVisible = createNewHosts(elements);
-    ngModule.destroy();
-    makeVisible();
   });
 };

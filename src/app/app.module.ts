d8d27cc1d0772a81a646e60247bdf9a1173e00fe
maktarsis <tarsis.maksym@gmail.com>
2018-09-03 10:29:16 +0300

    refactor: more consistency between core and app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,58 +5,22 @@ import {
 } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { environment } from '@env/environment';
 import { CoreModule } from '@app/core/core.module';
 import { SharedModule } from '@app/shared';
 import { appRouting } from '@app/app.routes';
 import { AppComponent } from '@app/app.component';
 
-import {
-  MetaReducer,
-  StoreModule
-} from '@ngrx/store';
-import {
-  StoreRouterConnectingModule,
-  RouterStateSerializer
-} from '@ngrx/router-store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { storeFreeze } from 'ngrx-store-freeze';
-import {
-  reducers,
-  CustomSerializer
-} from '@shared/store/reducers/states/router.reducers';
-import { routerEffects } from '@shared/store';
-
-export const metaReducers: MetaReducer<any>[] = !environment.production
-    ? [storeFreeze]
-    : [];
-
 @NgModule({
   imports: [
     BrowserModule.withServerTransition({ appId: 'app-root' }),
-    NoopAnimationsModule,
     BrowserTransferStateModule,
+    NoopAnimationsModule,
     appRouting,
     CoreModule,
-    SharedModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot(routerEffects),
-    StoreRouterConnectingModule,
-    StoreDevtoolsModule.instrument({
-      name: 'concept store platform',
-      logOnly: !environment.production
-    })
-
+    SharedModule
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent],
-  providers: [
-    {
-      provide: RouterStateSerializer,
-      useClass: CustomSerializer
-    }
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,24 +7,59 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
+import {
+  MetaReducer,
+  StoreModule
+} from '@ngrx/store';
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer
+} from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
+import {
+  reducers,
+  CustomSerializer
+} from '@shared/store/reducers/states/router.reducers';
+import { routerEffects } from '@shared/store';
+
+import { environment } from '@env/environment';
+import { MaterialModule } from '@material/material.module';
+import { MyApolloModule } from '@apollo/apollo.module';
 import { AuthModule } from '@auth/auth.module';
 import { CartModule } from '@cart/cart.module';
-import { MyApolloModule } from '@apollo/apollo.module';
-import { MaterialModule } from '@material/material.module';
 import { HeaderComponent } from '@core/components/header/header.component';
+
+export const metaReducers: MetaReducer<any>[] = !environment.production
+    ? [storeFreeze]
+    : [];
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule,
     RouterModule,
     HttpClientModule,
+    MaterialModule,
+    MyApolloModule,
     AuthModule,
     CartModule,
-    MyApolloModule
+    EffectsModule.forRoot(routerEffects),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreRouterConnectingModule,
+    StoreDevtoolsModule.instrument({
+      name: 'concept store platform',
+      logOnly: !environment.production
+    })
   ],
   declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  providers: [
+    {
+      provide: RouterStateSerializer,
+      useClass: CustomSerializer
+    }
+  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() core: CoreModule) {

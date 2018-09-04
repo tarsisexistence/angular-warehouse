43fb7e31060a02b615eeae326305569e65ad67f5
maktarsis <tarsis.maksym@gmail.com>
2018-09-04 19:51:30 +0300

    feat: add gql query test

--- src/app/apollo/services/apollo.service.spec.ts
+++ src/app/apollo/services/apollo.service.spec.ts
@@ -4,17 +4,48 @@ import {
 } from '@angular/core/testing';
 
 import { Apollo } from 'apollo-angular';
+import {
+  ApolloTestingController,
+  ApolloTestingModule
+} from 'apollo-angular/testing';
 
 import { ApolloService } from '@apollo/services/apollo.service';
+import { Order } from '@shared/interfaces/order.interface';
+import { allOrders } from '@apollo/queries';
 
 describe('ApolloService', () => {
+  let backend: ApolloTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [ApolloTestingModule],
       providers: [Apollo, ApolloService]
     });
+
+    backend = TestBed.get(ApolloTestingController);
   });
 
   it('should be created', inject([ApolloService], (service: ApolloService) => {
     expect(service).toBeTruthy();
+
+    it('should test getting all orders', (done) => {
+      const data = {
+        'data': {
+          'allOrders': [
+            {
+              'id': '197dab70-b062-11e8-8240-bfefe9f77f98',
+              'name': 'Tak Marsis',
+              'phone': '3809342112319'
+            }
+          ]
+        }
+      };
+      service.getAllOrders().subscribe((result: Order[]) => {
+        expect(result).toEqual(data);
+        done();
+      });
+
+      backend.expectOne(allOrders).flush(data);
+    });
   }));
 });

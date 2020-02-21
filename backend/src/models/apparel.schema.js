bc3d6e12ca83d9038b0408e439f0acd68f636b8a
maktarsis <tarsis.maksym@gmail.com>
2020-02-21 07:57:07 +0200

    fix(backend): colors prop in schema

--- backend/src/models/apparel.schema.js
+++ backend/src/models/apparel.schema.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 export const apparelSchema = new Schema({
   id: { type: String, default: uuid.v1 },
   title: String,
-  colors: String,
+  colors: [String],
   price: Number,
   image: String,
   type: String

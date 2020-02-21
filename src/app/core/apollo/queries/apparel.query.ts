bfb3310d73bfbc9c9eb119819b05c86f33d3322c
maktarsis <tarsis.maksym@gmail.com>
2020-02-21 07:48:44 +0200

    chore: update client side

--- src/app/core/apollo/queries/apparel.query.ts
+++ src/app/core/apollo/queries/apparel.query.ts
@@ -1,16 +1,14 @@
 import gql from 'graphql-tag';
 
 export const allApparel = gql`
-    query allApparel {
-        allApparel {
-            id
-            title
-            attribute
-            color
-            description
-            price
-            image
-            type
-        }
+  query allApparel {
+    allApparel {
+      id
+      title
+      colors
+      price
+      image
+      type
     }
+  }
 `;

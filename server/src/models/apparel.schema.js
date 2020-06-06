3c6af89841240b61e5ef1cf841c0972cfcb006d2
maktarsis <tarsis.maksym@gmail.com>
2020-06-06 11:41:59 +0300

    chore: bump uuid to the latest 8.1.0

--- server/src/models/apparel.schema.js
+++ server/src/models/apparel.schema.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import uuid from 'uuid';
+import { v1 } from 'uuid';
 
 const Schema = mongoose.Schema;
 export const apparelSchema = new Schema({
-  id: { type: String, default: uuid.v1 },
+  id: { type: String, default: v1 },
   title: String,
   colors: [String],
   price: Number,
--- server/src/models/user.schema.js
+++ server/src/models/user.schema.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import uuid from 'uuid';
+import { v1 } from 'uuid';
 
 const Schema = mongoose.Schema;
 export const userSchema = new Schema({
-  id: { type: String, default: uuid.v1 },
+  id: { type: String, default: v1 },
   email: { type: String, unique: true },
   password: String,
   catchPhrase: String
